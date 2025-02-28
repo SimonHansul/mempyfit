fca9ad519b1f87a395f8c337d2b89e728ff81aae
Simon Hansul <simonhansul@gmail.com>
2025-02-28 23:39:56 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mempyDEB',
+    version='0.1.0',
+    description='DEB-TKTD modelling in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Simon Hansul',
+    author_email='simonhansul@gmail.com',
+    url='https://github.com/simonhansul/mempyDEB',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'mesa',
+        'tqdm',
+        'yml',
+        'collections'
+        ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
