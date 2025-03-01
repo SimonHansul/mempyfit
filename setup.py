4b87a57a4b947187d7a547848d36f42d804d5fd7
SimonHansul <simon.hansul@gmail.com>
2025-03-01 13:48:33 +0100

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mempyfit',
-    version='0.1.0',
+    version='0.1.1',
     description='Fitting DEB-TKTD models in Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
