a35ddf53339e75df97d21d19747cc2e9bd95a843
Simon Hansul <hansul@gaiac-eco.de>
2026-08-18 16:57:35 +0200

    fix pyabc reporting error

--- mempyfit/backend_pyabc.py
+++ mempyfit/backend_pyabc.py
@@ -348,8 +348,8 @@ class pyABCBackend(FittingBackend):
             for (i,par) in enumerate(self.priors.keys()):
 
                 # FIXME: something is going wrong here...maybe a good idea to simplify the visualization anyhow.
-                xmin = self.priors[i].ppf(0.0001)
-                xmax = self.priors[i].ppf(0.9999)
+                xmin = self.priors[par].ppf(0.0001)
+                xmax = self.priors[par].ppf(0.9999)
 
                 pyabc.visualization.plot_kde_1d(
                     df,
